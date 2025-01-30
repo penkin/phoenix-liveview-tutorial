f76ce5b7ccc8e8b0fda1b6b3297b981fbc8534f3
Christopher Penkin <chris@penkin.me>
2025-01-30 20:52:53 +0200

    Part 9

--- lib/werdle_web/live/word_live/index.ex
+++ lib/werdle_web/live/word_live/index.ex
@@ -34,17 +34,17 @@ defmodule WerdleWeb.WordLive.Index do
 
     with {:ok, _changeset} <- Game.validate_guess(changeset, guess_row),
          {:correct, _changeset} <- Game.check_guess_correctness(changeset, guess_row, solve) do
-      {:noreply, socket}
+      {:noreply, handle_correct_guess(socket, solve)}
     else
-      {:error, _error_message} ->
-        {:noreply, socket}
+      {:error, error_message} ->
+        {:noreply, handle_invalid_guess(socket, error_message)}
 
       {:incorrect, _changeset}  when guess_row == 5 ->
-        {:noreply, socket}
+        {:noreply, handle_game_over(socket, solve)}
 
       {:incorrect, _changeset} ->
         socket = assign(socket, :current_guess, guess_row + 1)
-        {:noreply, socket}
+        {:noreply, handle_incorrect_guess(socket, guess_row)}
     end
   end
 
@@ -58,5 +58,26 @@ defmodule WerdleWeb.WordLive.Index do
       socket = assign(socket, :changeset, Game.update_guesses(changeset, key, guess_row))
       {:noreply, socket}
     end
+  end
+
+  defp handle_correct_guess(socket, solve) do
+    socket
+    |> put_flash(:correct, "#{String.upcase(solve.name)} is correct!")
+  end
+
+  defp handle_invalid_guess(socket, error_message) do
+    socket
+    |> put_flash(:error, error_message)
+  end
+
+  defp handle_game_over(socket, solve) do
+    socket
+    |> put_flash(:game_over, "#{String.upcase(solve.name)} was the correct answer.")
+  end
+
+  defp handle_incorrect_guess(socket, guess_row) do
+    socket
+    |> assign(:current_guess, guess_row + 1)
+    |> put_flash(:incorrect, "Try again.")
   end
 end
